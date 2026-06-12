b4c58d6536d9e478823d5455022709158dd812e8
Oanh1108 <kieuoanh10824@gmail.com>
2026-06-12 23:39:17 +0700

    todolist

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,15 +6,28 @@ import { connectDB } from './config/data.js';
 dns.setServers(["1.1.1.1","8.8.8.8"])
 import taskRoute from './routes/taskRoute.js'
 import cors from 'cors'
+import path from 'path'
 
 const app = express();
 
 const PORT = process.env.PORT || 5001;
+const _dirname = path.resolve();
 
 app.use(express.json());
-app.use(cors({origin: ["http://localhost:5173","http://localhost:5174"]}))
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({origin: ["http://localhost:5173","http://localhost:5174"]}))
+}
 
 app.use("/api/tasks", taskRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
